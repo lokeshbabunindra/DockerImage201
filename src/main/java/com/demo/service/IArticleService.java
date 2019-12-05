5727ee1bfb1ecd9cb727283391db1be113ffeecc
Charishma Pemmasani <M1048143@mindtree.com>
2019-12-05 15:29:31 +0530

    code pushed

---
+++ src/main/java/com/demo/service/IArticleService.java
@@ -0,0 +1,13 @@
+package com.demo.service;
+
+import java.util.List;
+
+import com.demo.entity.Article;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}
